12ea6ecde5b456047941d57ce24612126022fa46
Pascal Wichmann <wichmannpas@gmail.com>
2016-11-02 15:37:38 +0100

    Fixed url patterns (added consistent trailing slash)

--- dudel/base/urls.py
+++ dudel/base/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login$', views.login, name='login'),
+    url(r'^login/$', views.login, name='login'),
 ]
--- dudel/invitations/urls.py
+++ dudel/invitations/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^([a-zA-Z0-9_\-]+)$', views.ivitations, name='invitations'),
-    url(r'^([a-zA-Z0-9_\-]+)/(\d+)/delete$', views.delete_invitation, name='invitations_deleteInvitation'),
-    url(r'^([a-zA-Z0-9_\-]+)/(\d+)/resend$', views.resend_invitation, name='invitations_resendInvitation'),
-    url(r'^([a-zA-Z0-9_\-]+)/resend$', views.resend, name='invitations_resend'),
+    url(r'^([a-zA-Z0-9_\-]+)/$', views.ivitations, name='invitations'),
+    url(r'^([a-zA-Z0-9_\-]+)/(\d+)/delete/$', views.delete_invitation, name='invitations_deleteInvitation'),
+    url(r'^([a-zA-Z0-9_\-]+)/(\d+)/resend/$', views.resend_invitation, name='invitations_resendInvitation'),
+    url(r'^([a-zA-Z0-9_\-]+)/resend/$', views.resend, name='invitations_resend'),
 ]
