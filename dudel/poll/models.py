9b4566eb90f0dcceef481109dca84c83e409e3cb
Mareike Schmidt <1mschmid@informatik.uni-hamburg.de>
2017-02-23 15:00:01 +0100

    list of owned polls implemented

--- dudel/poll/models.py
+++ dudel/poll/models.py
@@ -25,6 +25,8 @@ class Poll(models.Model):
     url = models.CharField(max_length=80, unique=True)
     type = models.CharField(max_length=20, choices=POLL_TYPES, default="universal")
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, null=True, blank=True)
+    group = models.ForeignKey(Group, null=True, blank=True)
     """owner_id = models.ForeignKey(Member)"""
 
     # === Extra settings ==
