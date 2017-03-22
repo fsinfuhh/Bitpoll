59f22d1c1f3a94786ad542354635f7e92a7af1c7
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-03-22 17:40:38 +0100

    fix duplicated invitations in invitation related urls

--- dudel/invitations/urls.py
+++ dudel/invitations/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^invitations/([a-zA-Z0-9_\-]+)/$', views.invite, name='invitations'),
-    url(r'^invitations_send/([a-zA-Z0-9_\-]+)/$', views.invitation_send, name='invitations_send'),
+    url(r'^([a-zA-Z0-9_\-]+)/$', views.invite, name='invitations'),
+    url(r'^([a-zA-Z0-9_\-]+)/send/$', views.invitation_send, name='invitations_send'),
 ]
