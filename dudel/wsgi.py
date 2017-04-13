bfc832c864958e217358d1d136508d9bd58e21d5
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-04-13 15:28:37 +0200

    add sentry to wsgi.py to catch low level errors

--- dudel/wsgi.py
+++ dudel/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+try:
+    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+    raven_support = True
+except ImportError:
+    raven_support = False
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dudel.settings")
 
-application = get_wsgi_application()
+if raven_support:
+    application = Sentry(get_wsgi_application())
+else:
+    application = get_wsgi_application()
