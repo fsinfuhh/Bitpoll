44cd0183f1fc63a07bdc2a4fe4fbaca906308cbc
Mareike Schmidt <1mschmid@informatik.uni-hamburg.de>
2017-04-19 15:07:36 +0200

    assignment of user to vote

--- dudel/poll/views.py
+++ dudel/poll/views.py
@@ -83,12 +83,11 @@ def poll(request, poll_url):
     choice_idx = {choice.id: i for (i, choice) in enumerate(choices)}
 
     vote_choice_matrix = [[None] * len(choice_idx) for _ in vote_idx]
-    for vote_choice in VoteChoice.objects.filter(vote__poll=current_poll, choice__deleted=0).\
+    for vote_choice in VoteChoice.objects.filter(vote__poll=current_poll, choice__deleted=0). \
             select_related('value', 'choice__poll', 'choice'):
         x = vote_idx[vote_choice.vote_id]
         y = choice_idx[vote_choice.choice_id]
         vote_choice_matrix[x][y] = vote_choice
-
 
     # aggregate stats for the different Choice_Values per column
     stats2 = Choice.objects.filter(poll=current_poll, deleted=False).order_by('sort_key').annotate(
@@ -99,7 +98,7 @@ def poll(request, poll_url):
     stats = [
         {
             'score': (stat['score'] / Decimal(votes_count)
-                if votes_count > 0 else 0) if stat['score'] is not None else None,
+                      if votes_count > 0 else 0) if stat['score'] is not None else None,
             'count': stat['score'],
             'text': stat,
             'choices': [
@@ -108,8 +107,8 @@ def poll(request, poll_url):
                     'color': stat2['votechoice__value__color'],
                     'icon': stat2['votechoice__value__icon'],
                     'title': stat2['votechoice__value__title']} for
-                        stat2 in stats2 if
-                        stat2['id'] == stat['id'] and stat2['votechoice__value__color'] != None],
+                stat2 in stats2 if
+                stat2['id'] == stat['id'] and stat2['votechoice__value__color'] != None],
         } for stat in stats]
 
     poll_watched = False
@@ -139,7 +138,7 @@ def poll(request, poll_url):
         'watched': poll_watched,
         'comment_form': CommentForm(),
         'timezone_warning': (request.user.is_authenticated and
-            current_poll.get_tz_name(request.user) != request.user.timezone),
+                             current_poll.get_tz_name(request.user) != request.user.timezone),
     })
 
 
@@ -148,6 +147,7 @@ def comment(request, poll_url, comment_id=None):
     Post a comment to a poll
     :param request:
     :param poll_url: the poll url to post to
+    :param comment_id:
     :return:
     """
     current_poll = get_object_or_404(Poll, url=poll_url)
@@ -586,7 +586,6 @@ def edit_choicevalues(request, poll_url):
         )
         return redirect('poll', poll_url)
 
-
     if not current_poll.can_listen(request.user):
         messages.error(
             request, _("You are not allowed to listen.")
@@ -774,11 +773,12 @@ def vote(request, poll_url, vote_id=None):
                             new_choices.append(VoteChoice(value=choice_value,
                                                           vote=current_vote,
                                                           choice=choice,
-                                                          comment=request.POST.get('comment_{}'.format(choice.id)) or ''))
+                                                          comment=request.POST.get(
+                                                              'comment_{}'.format(choice.id)) or ''))
                         if vote_id:
                             VoteChoice.objects.filter(vote=current_vote).delete()
-                            #todo: nochmal prüfen ob das wirjklich das tut was es soll, also erst alles löschen und dann neu anlegen
-                            #todo eventuell eine transaktion drum machen? wegen falls das eventuell dazwischen abbricht?
+                            # todo: nochmal prüfen ob das wirjklich das tut was es soll, also erst alles löschen und dann neu anlegen
+                            # todo eventuell eine transaktion drum machen? wegen falls das eventuell dazwischen abbricht?
                         else:
                             for current_watch in current_poll.pollwatch_set.all():
                                 current_watch.send(request=request, vote=current_vote)
@@ -831,12 +831,30 @@ def vote(request, poll_url, vote_id=None):
         'page': 'Vote',
         'current_vote': current_vote,
         'timezone_warning': (request.user.is_authenticated and
-            current_poll.get_tz_name(request.user) != request.user.timezone),
+                             current_poll.get_tz_name(request.user) != request.user.timezone),
     })
 
 
 def vote_assign(request, poll_url, vote_id):
-    pass
+    current_poll = get_object_or_404(Poll, url=poll_url)
+    current_vote = get_object_or_404(Vote, id=vote_id)
+
+    if request.method == 'POST':
+        if request.user.is_authenticated and current_poll.can_edit(request.user):
+            username = request.POST.get('username').strip()
+            user = DudelUser.objects.get(username=username)
+            current_vote.user = user
+            current_vote.name = username
+            current_vote.save()
+
+            return redirect('poll', poll_url)
+        else:
+            return HttpResponseForbidden()
+
+    return TemplateResponse(request, 'poll/vote_assign.html', {
+        'poll': current_poll,
+        'vote': current_vote
+    })
 
 
 def vote_delete(request, poll_url, vote_id):
