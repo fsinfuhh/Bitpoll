3b8a839108c42f039a354f6a3824ce355117c3f1
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-04-28 15:11:42 +0200

    add optimising and error error corrections to it

--- dudel/poll/views.py
+++ dudel/poll/views.py
@@ -79,16 +79,18 @@ def poll(request, poll_url):
     if not summary:
         messages.info(request, _("The summary is not shown due to the config of the Poll"))
 
-    choices = Choice.objects.filter(poll=current_poll, deleted=0).order_by('sort_key')
+    choices = Choice.objects.filter(poll=current_poll, deleted=0).select_related('poll').order_by('sort_key')
     vote_idx = {vote.id: i for (i, vote) in enumerate(poll_votes)}
-    choice_idx = {choice.id: i for (i, choice) in enumerate(choices)}
+    choice_idx = {choice.id: (i, choice) for (i, choice) in enumerate(choices)}
 
     vote_choice_matrix = [[None] * len(choice_idx) for _ in vote_idx]
-    for vote_choice in VoteChoice.objects.filter(vote__poll=current_poll, choice__deleted=0). \
-            select_related('value', 'choice__poll', 'choice'):
-        x = vote_idx[vote_choice.vote_id]
-        y = choice_idx[vote_choice.choice_id]
-        vote_choice_matrix[x][y] = vote_choice
+    for vote_choice in VoteChoice.objects.filter(vote__poll=current_poll, choice__deleted=0).select_related('value'):
+        if vote_choice.vote_id not in vote_idx:
+            x = vote_idx[vote_choice.vote_id]
+            y, choice = choice_idx[vote_choice.choice_id]
+            vote_choice_matrix[x][y] = {'comment': vote_choice.comment,
+                                        'value': vote_choice.value,
+                                        'choice': choice}
 
     # aggregate stats for the different Choice_Values per column
     stats2 = Choice.objects.filter(poll=current_poll, deleted=False).order_by('sort_key').annotate(
