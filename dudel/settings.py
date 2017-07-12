f854f04a910095a69ff9ae23a03afe9eca5fcee1
Pascal Wichmann <git@pa-w.de>
2017-07-12 15:57:38 +0200

    Split settings into default and local settings

--- dudel/settings.py
+++ dudel/settings.py
@@ -167,7 +167,7 @@ AUTH_USER_MODEL = 'base.DudelUser'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -219,40 +219,9 @@ LANGUAGES = (
 REGISTER_ENABLED = True
 GROUP_MANAGEMENT = REGISTER_ENABLED
 
-"""
-import ldap
-from django_auth_ldap.config import LDAPSearch
-
-AUTHENTICATION_BACKENDS = (
-    'django_auth_ldap.backend.LDAPBackend',
-    'django.contrib.auth.backends.ModelBackend',
-    )
-
-AUTH_LDAP_SERVER_URI = "ldap_host"
-AUTH_LDAP_BIND_DN = "ldap_bind_dn"
-AUTH_LDAP_BIND_PASSWORD = "ldap_bind_pw"
-AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=mafiasi,dc=de",
-    ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
-AUTH_LDAP_ALWAYS_UPDATE_USER = True
-
-from django_auth_ldap.config import LDAPSearch, PosixGroupType
-
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=groups,dc=mafiasi,dc=de",
-    ldap.SCOPE_SUBTREE, "(objectClass=posixGroup)"
-    )
-AUTH_LDAP_GROUP_TYPE = PosixGroupType()
-#AUTH_LDAP_FIND_GROUP_PERMS = True
-AUTH_LDAP_MIRROR_GROUPS = True
-
-AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn", "email": "mail"}
-
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    "is_staff": ["cn=Editoren,ou=groups,dc=mafiasi,dc=de",
-                 "cn=Server-AG,ou=groups,dc=mafiasi,dc=de"],
-    "is_superuser": "cn=Server-AG,ou=groups,dc=mafiasi,dc=de"
-}
-"""
 
 MAIL_SIGNATURE = "Dudel Team"
 
 TEAM_EMAIL = "mail@example.com"
+
+from .settings_local import *
---
+++ dudel/settings_local.py
@@ -0,0 +1,73 @@
+# Add your production settings here
+
+#LANGUAGE_CODE = 'en-us'
+#
+#TIME_ZONE = 'Europe/Berlin'
+#
+## Database
+## https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+## Url to the Base Homepage and Text on the Link, leave empty to not use this option
+#HOME_URL = "https://example.com"
+#HOME_URL_NAME = "Dashboard"
+#
+## Test mail functionality by printing mails to console:
+## EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#
+## if the imprint URL is not empty use it as an link to the imprint, else use IMPRINT_TEXT
+#IMPRINT_URL = ""
+#IMPRINT_TEXT = """
+#<h1>ImpressuXm</h1>
+#<p>Text goes here</p>
+#"""
+#
+#LOCALE_PATHS = (os.path.join(ROOT_DIR, 'locale'), )
+#
+#LANGUAGES = (
+#    ('de', 'Deutsch'),
+#    ('en', 'English'),
+#    #('fr', 'Français'),
+#)
+
+#REGISTER_ENABLED = True
+#GROUP_MANAGEMENT = REGISTER_ENABLED
+
+# Use ldap login
+#import ldap
+#from django_auth_ldap.config import LDAPSearch
+#
+#AUTHENTICATION_BACKENDS = (
+#    'django_auth_ldap.backend.LDAPBackend',
+#    'django.contrib.auth.backends.ModelBackend',
+#    )
+#
+#AUTH_LDAP_SERVER_URI = "ldap_host"
+#AUTH_LDAP_BIND_DN = "ldap_bind_dn"
+#AUTH_LDAP_BIND_PASSWORD = "ldap_bind_pw"
+#AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=mafiasi,dc=de",
+#    ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+#AUTH_LDAP_ALWAYS_UPDATE_USER = True
+#
+#from django_auth_ldap.config import LDAPSearch, PosixGroupType
+#
+#AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=groups,dc=mafiasi,dc=de",
+#    ldap.SCOPE_SUBTREE, "(objectClass=posixGroup)"
+#    )
+#AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+##AUTH_LDAP_FIND_GROUP_PERMS = True
+#AUTH_LDAP_MIRROR_GROUPS = True
+#
+#AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn", "email": "mail"}
+#
+#AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+#    "is_staff": ["cn=Editoren,ou=groups,dc=mafiasi,dc=de",
+#                 "cn=Server-AG,ou=groups,dc=mafiasi,dc=de"],
+#    "is_superuser": "cn=Server-AG,ou=groups,dc=mafiasi,dc=de"
+#}
