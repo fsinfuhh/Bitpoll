0c948bfab2e6c37dee3e312f22fd3333d2e6d5c7
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2018-09-14 22:02:14 +0200

    registration: make it Django 2.1 compatible
    some cleanups, and inconsistend password checks are removed

--- bitpoll/registration/urls.py
+++ bitpoll/registration/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls import url
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm,\
-    password_reset_complete
-
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
 from .forms import PasswordResetForm
 from .views import *
@@ -11,29 +8,29 @@ from .views import *
 
 urlpatterns = [
     url(r'^change_email/([a-zA-Z0-9:_-]+)$', change_email,
-            name='registration_change_email'),
+        name='registration_change_email'),
     url(r'^request_successful/([a-zA-Z0-9.\-_+]+@[a-zA-Z0-9.\-_]+)$', request_successful,
-            name='registration_request_successful'),
+        name='registration_request_successful'),
     url(r'^account$', account_settings, name='registration_account'),
-    ]
+]
 
-if settings.REGISTER_ENABLED:  # TODO: own settings value for changing password? If yes: remember to change template ifs when to show the link
+# TODO: own settings value for changing password? If yes: remember to change template ifs when to show the link
+if settings.REGISTER_ENABLED:
     urlpatterns += [
-        url(r'^password_reset/$', password_reset, {
+        url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), {
             'password_reset_form': PasswordResetForm
         }, name='password_reset'),
-        url(r'^password_reset/done$', password_reset_done,
+        url(r'^password_reset/done$', auth_views.PasswordResetDoneView.as_view(),
             name='password_reset_done'),
-        url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)$',
-            password_reset_confirm, name='password_reset_confirm'),
-        url(r'^password_reset/complete', password_reset_complete,
+        path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+        url(r'^password_reset/complete', auth_views.PasswordResetCompleteView.as_view(),
             name='password_reset_complete')
-]
+    ]
 
 if settings.REGISTER_ENABLED:
     urlpatterns += [
         url(r'^request_account$', request_account,
-                name='registration_request_account'),
+            name='registration_request_account'),
         url(r'^create_account/([a-zA-Z0-9:_-]+)$', create_account,
-                name='registration_create_account'),
+            name='registration_create_account'),
     ]
