3b0215865e4f2f2f971dfb0ae4e4e9406517450c
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2019-05-31 22:26:46 +0200

    fix redirection on logout

--- bitpoll/urls.py
+++ bitpoll/urls.py
@@ -28,9 +28,7 @@ urlpatterns = [
     url(r'^invitations/', include('bitpoll.invitations.urls')),
     url(r'^$', lambda req: redirect('index'), name='home'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login', ),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), {
-        'next_page': '/'
-    }, name='logout'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     url(r'^markdown/', include('django_markdown.urls')),
     url(r'^registration/', include('bitpoll.registration.urls')),
 
