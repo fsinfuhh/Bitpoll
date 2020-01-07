71e1bdb410cbb34a2513909ba5fd2dea24c63919
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2020-01-07 19:20:15 +0100

    poll.urls.py: migrate from url() to path()

--- bitpoll/poll/urls.py
+++ bitpoll/poll/urls.py
@@ -1,33 +1,43 @@
 from django.conf.urls import url
+from django.urls import include, path
 
 from . import views
 
+#app_name = 'poll'
+
 urlpatterns = [
-    url(r'^([a-zA-Z0-9_\-]+)/$', views.poll, name='poll'),
-    url(r'^([a-zA-Z0-9_\-]+).csv$', views.poll, {'export': True}, name='poll_export_csv'),
-    url(r'^([a-zA-Z0-9_\-]+)/comment/$', views.comment, name='poll_comment'),
-    url(r'^([a-zA-Z0-9_\-]+)/comment/(\d+)/edit/$', views.comment, name='poll_comment_edit'),
-    url(r'^([a-zA-Z0-9_\-]+)/comment/(\d+)/delete/$', views.delete_comment, name='poll_deleteComment'),
-    url(r'^([a-zA-Z0-9_\-]+)/watch/$', views.watch, name='poll_watch'),
-    url(r'^([a-zA-Z0-9_\-]+)/settings/$', views.settings, name='poll_settings'),
+    path('<slug:poll_url>.csv', views.poll, {'export': True}, name='poll_export_csv'),
+    path('<slug:poll_url>/', include([
+        path('', views.poll, name='poll'),
+        path('comment/', views.comment, name='poll_comment'),
+        path('comment/<int:comment_id>/edit/', views.comment, name='poll_comment_edit'),
+        path('comment/<int:comment_id>/delete/', views.delete_comment, name='poll_deleteComment'),
 
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choices/$', views.edit_choice, name='poll_editChoice'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choices/date/$', views.edit_date_choice, name='poll_editDateChoice'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choices/dateTime/date/$', views.edit_dt_choice_date, name='poll_editDTChoiceDate'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choices/dateTime/time/$', views.edit_dt_choice_time, name='poll_editDTChoiceTime'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choices/dateTime/combinations/$', views.edit_dt_choice_combinations,
-        name='poll_editDTChoiceCombinations'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choices/universal/$', views.edit_universal_choice, name='poll_editUniversalChoice'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choicevalues/', views.edit_choicevalues, name='poll_editchoicevalues'),
-    url(r'^([a-zA-Z0-9_\-]+)/edit/choicevalues_create', views.edit_choicevalues_create,
-        name='poll_editchoicevalues_create'),
+        path('watch/', views.watch, name='poll_watch'),
+        path('settings/', views.settings, name='poll_settings'),
 
-    url(r'^([a-zA-Z0-9_\-]+)/delete/$', views.delete, name='poll_delete'),
+        path('edit/', include([
+            path('choices/', views.edit_choice, name='poll_editChoice'),
+            path('choices/date/', views.edit_date_choice, name='poll_editDateChoice'),
+            path('choices/dateTime/date/', views.edit_dt_choice_date, name='poll_editDTChoiceDate'),
+            path('choices/dateTime/time/', views.edit_dt_choice_time, name='poll_editDTChoiceTime'),
+            path('choices/dateTime/combinations/', views.edit_dt_choice_combinations,
+                name='poll_editDTChoiceCombinations'),
+            path('choices/universal/', views.edit_universal_choice, name='poll_editUniversalChoice'),
 
-    url(r'^([a-zA-Z0-9_\-]+)/vote/$', views.vote, name='poll_vote'),
-    url(r'^([a-zA-Z0-9_\-]+)/vote/(\d+)/assign/$', views.vote_assign, name='poll_voteAssign'),
-    url(r'^([a-zA-Z0-9_\-]+)/vote/(\d+)/edit/$', views.vote, name='poll_voteEdit'),
-    url(r'^([a-zA-Z0-9_\-]+)/vote/(\d+)/delete/$', views.vote_delete, name='poll_voteDelete'),
+            path('choicevalues/', views.edit_choicevalues, name='poll_editchoicevalues'),
+            path('choicevalues_create/', views.edit_choicevalues_create,
+                name='poll_editchoicevalues_create'),
+        ])),
+        path('delete/', views.delete, name='poll_delete'),
 
-    url(r'^([a-zA-Z0-9_\-]+)/copy/$', views.copy, name='poll_copy'),
+        path('vote/', views.vote, name='poll_vote'),
+        path('vote/<int:vote_id>/', include([
+            path('assign/', views.vote_assign, name='poll_voteAssign'),
+            path('edit/', views.vote, name='poll_voteEdit'),
+            path('delete/', views.vote_delete, name='poll_voteDelete'),
+        ])),
+        path('copy/', views.copy, name='poll_copy'),
+    ])),
+
 ]
