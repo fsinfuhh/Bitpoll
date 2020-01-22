a313afe0f6b5802c0b334385cdbfd566bb81043a
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2020-01-22 17:24:33 +0100

    groups code to spectre and forms

--- bitpoll/groups/urls.py
+++ bitpoll/groups/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url
+from django.urls import path, include, re_path
 
+from .models import group_name_regex
 from .views import *
 
 urlpatterns = [
-    url(r'^$', index, name='groups_index'),
-    url(r'^create$', create, name='groups_create'),
-    url(r'^g/([a-zA-Z0-9_-]+)/$', show, name='groups_show'),
-    url(r'^g/([a-zA-Z0-9_-]+)/leave$', leave, name='groups_leave'),
-    url(r'^g/([a-zA-Z0-9_-]+)/invite$', invite, name='groups_invite'),
-    url(r'^g/([a-zA-Z0-9_-]+)/action/(\d+)$', group_action,
+    path('', index, name='groups_index'),
+    path('create', create, name='groups_create'),
+    re_path('^g/(?P<group_name>{})/'.format(group_name_regex), include([
+        path('', show, name='groups_show'),
+        path('leave', leave, name='groups_leave'),
+        path('invite', invite, name='groups_invite'),
+        path('action/<int:member_pk>', group_action,
             name='groups_action'),
-    url(r'^invitations/(\d+)/action', invitation_action,
+        ])),
+    path('invitations/<int>/action', invitation_action,
             name='groups_invitation_action'),
-    url(r'^invitations/(\d+)/withdraw', withdraw_invite,
+    path('invitations/<int>/withdraw', withdraw_invite,
             name='groups_withdraw_invite'),
 ]
