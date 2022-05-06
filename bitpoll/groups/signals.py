3d8a3f71fbae06645586df6dbdf4a3bc7c652450
Nils Rokita <github@rokita.it>
2022-05-07 00:33:55 +0200

    fixes in config, signals, add migrations

--- bitpoll/groups/signals.py
+++ bitpoll/groups/signals.py
@@ -3,5 +3,4 @@ from django.dispatch import Signal
 class GroupOverview(object):
     pass
 
-show_group_overview = Signal(providing_args=['group', 'members',
-                                             'is_admin', 'is_member'])
+show_group_overview = Signal()
