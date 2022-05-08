50b3d96703d32442915ec3d7567c9d5e5628f6e2
Nils Rokita <github@rokita.it>
2022-05-08 16:06:32 +0200

    Add ability to escape choice grouping, fixes #62

--- bitpoll/poll/util.py
+++ bitpoll/poll/util.py
@@ -28,3 +28,40 @@ class PartialDateTime(object):
             return date_format(self.datetime, format='H:i')
         elif self.part == DateTimePart.datetime:
             return date_format(self.datetime, format='D, j. N Y H:i')
+
+
+#From: https://stackoverflow.com/questions/18092354/python-split-string-without-splitting-escaped-character
+def split_unescape(s, delim, escape='\\', unescape=True):
+    """
+    >>> split_unescape('foo,bar', ',')
+    ['foo', 'bar']
+    >>> split_unescape('foo$,bar', ',', '$')
+    ['foo,bar']
+    >>> split_unescape('foo$$,bar', ',', '$', unescape=True)
+    ['foo$', 'bar']
+    >>> split_unescape('foo$$,bar', ',', '$', unescape=False)
+    ['foo$$', 'bar']
+    >>> split_unescape('foo$', ',', '$', unescape=True)
+    ['foo$']
+    """
+    ret = []
+    current = []
+    itr = iter(s)
+    for ch in itr:
+        if ch == escape:
+            try:
+                # skip the next character; it has been escaped!
+                if not unescape:
+                    current.append(escape)
+                current.append(next(itr))
+            except StopIteration:
+                if unescape:
+                    current.append(escape)
+        elif ch == delim:
+            # split! (add current to the list and reset it)
+            ret.append(''.join(current))
+            current = []
+        else:
+            current.append(ch)
+    ret.append(''.join(current))
+    return ret
