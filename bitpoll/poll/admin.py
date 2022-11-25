822ae93c10574c65467024ba9a5fd3d328b01c7b
Tom Kunze <tom.kunze@fau.de>
2022-11-25 19:19:02 +0100

    poll/admin: Set list_display and search_fields for Poll and Comment

--- bitpoll/poll/admin.py
+++ bitpoll/poll/admin.py
@@ -3,11 +3,20 @@ from django.contrib import admin
 from .models import Poll, Choice, ChoiceValue, Comment, PollWatch, Vote, VoteChoice
 
 
-# Register your models here.
-admin.site.register(Poll)
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'user', 'created')
+    search_fields = ('title', 'url', 'user__username')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'poll', 'user', 'date_created')
+    search_fields = ('text', 'name', 'user__username', 'poll__title')
+
+
 admin.site.register(Choice)
 admin.site.register(ChoiceValue)
-admin.site.register(Comment)
 admin.site.register(PollWatch)
 admin.site.register(Vote)
 admin.site.register(VoteChoice)
